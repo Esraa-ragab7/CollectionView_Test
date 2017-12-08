ebea95bb04ab84b15b3675fe83393cf7046f4f53
Esraa Ragab <esraa.ragab@Tagipedia.com>
2017-12-08 21:57:35 +0200

    Initial Commit

---
+++ CollectionView_Task/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView_Task
+//
+//  Created by Tagipedia on 12/8/17.
+//  Copyright © 2017 EssO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionView_Task/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionView_Task
+//
+//  Created by Tagipedia on 12/8/17.
+//  Copyright © 2017 EssO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
